1dd954a3a01c765ee3a1d02a1619e0a720dabb5c
GUILHERME GUEDERT <guilherme_guedert@estudante.sesisenai.org.br>
2025-03-14 12:43:09 -0300

    jogo pronto

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 const canvas = document.getElementById('jogo2D');
 const ctx = canvas.getContext('2d');
 let gameOver = false;
+let personagem;
+let obstaculo;
 
+function iniciarJogo() {
+    personagem = new Personagem(100, canvas.height - 50, 50, 50);
+    obstaculo = new Obstaculo(canvas.width, canvas.height - 70, 50, 70, 3);
+    gameOver = false;
+    loop();
+}
+
+function reiniciarJogo() {
+    setTimeout(() => {
+        iniciarJogo();
+    }, 100); // Pequeno delay para evitar problemas com o alert
+}
 
 document.addEventListener('keypress', (e) => {
     if (e.code === 'Space' && !personagem.pulando) {
-        personagem.saltar()
+        personagem.saltar();
     }
 });
 
-class Entidade{
-    #gravidade
-    constructor(x,y, largura, altura){
+class Entidade {
+    #gravidade;
+    constructor(x, y, largura, altura) {
         this.x = x;
         this.y = y;
         this.largura = largura;
@@ -19,56 +33,68 @@ class Entidade{
         this.#gravidade = 0.5;
     }
 
-    get gravidade(){
-        return this.#gravidade
+    get gravidade() {
+        return this.#gravidade;
     }
 
-    desenhar = function(cor){
+    desenhar(cor) {
         ctx.fillStyle = cor;
-        ctx.fillRect(this.x,this.y,this.largura,this.altura);
+        ctx.fillRect(this.x, this.y, this.largura, this.altura);
     }
 }
 
-class Personagem extends Entidade{
-    #pulando
-    #velocidadeY
-    constructor (x,y,largura, altura){
-        super(x,y,altura, largura)
-        this.#pulando = false
-        this.#velocidadeY = 0
+class Personagem extends Entidade {
+    #pulando;
+    #velocidadeY;
+    constructor(x, y, largura, altura) {
+        super(x, y, largura, altura);
+        this.#pulando = false;
+        this.#velocidadeY = 0;
     }
-    saltar = function(){
-        this.#velocidadeY = 12;
+    saltar() {
+        this.#velocidadeY = 15;
         this.#pulando = true;
     }
     get pulando() {
-        return this.#pulando
+        return this.#pulando;
     }
     atualizarPersonagem() {
         if (this.#pulando) {
             this.#velocidadeY -= this.gravidade;
-            this.y -= this.#velocidadeY 
+            this.y -= this.#velocidadeY;
             if (this.y >= canvas.height - 50) {
-                this.#velocidadeY = 0
-                this.#pulando = false
-                this.y = canvas.height-50
+                this.#velocidadeY = 0;
+                this.#pulando = false;
+                this.y = canvas.height - 50;
             }
+        }
+    }
+    verificarColisao() {
+        let margem = 5;
+        if (
+            this.x < obstaculo.x + obstaculo.largura - margem &&
+            this.x + this.largura - margem >= obstaculo.x &&
+            this.y < obstaculo.y + obstaculo.altura - margem &&
+            this.y + this.altura - margem >= obstaculo.y
+        ) {
+            gameOver = true;
+            alert("Game Over!");
+            reiniciarJogo();
         }
     }
 }
 
-
-class Obstaculo extends Entidade{
-    constructor (x,y,largura, altura,velocidadeX, gravidade){
-        super(x,y, altura, largura, gravidade)
-        this.velocidadeX = velocidadeX
+class Obstaculo extends Entidade {
+    constructor(x, y, largura, altura, velocidadeX) {
+        super(x, y, largura, altura);
+        this.velocidadeX = velocidadeX;
     }
     atualizarObstaculo() {
-        this.x -= this.velocidadeX;    
+        this.x -= this.velocidadeX;
         if (this.x <= -this.largura) {
             this.x = canvas.width;
-            this.velocidadeX = Math.min(this.velocidadeX + 1, 10);
-    
+            this.velocidadeX = Math.min(this.velocidadeX + 4, 10);
+
             let nova_altura = Math.random() * 50 + 100;
             this.altura = nova_altura;
             this.y = canvas.height - nova_altura;
@@ -76,31 +102,16 @@ class Obstaculo extends Entidade{
     }
 }
 
-const obstaculo = new Obstaculo(
-    canvas.width, // Começa no lado direito da tela
-    canvas.height - 50, // No chão
-    30, // Largura do obstáculo
-    50, // Altura do obstáculo
-    3 // Velocidade
-)
-
-const personagem = new Personagem
-(
-    100,
-    canvas.height - 50,
-    50,
-    50,
-)
-
 function loop() {
-    if (gameOver == false) {
+    if (!gameOver) {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        personagem.desenhar('black')
-        personagem.atualizarPersonagem()
-        obstaculo.desenhar('red')
-        obstaculo.atualizarObstaculo()
-        requestAnimationFrame(loop);       
+        personagem.desenhar('black');
+        personagem.atualizarPersonagem();
+        obstaculo.desenhar('red');
+        obstaculo.atualizarObstaculo();
+        personagem.verificarColisao();
+        requestAnimationFrame(loop);
     }
 }
 
-loop()
+iniciarJogo();
